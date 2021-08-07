fc7fdbab3d3c9c8a300a5e682bd65a1d5c2c0c2c
RADAJO2 <radajo2@oocl.com>
2021-08-07 14:55:34 +0800

    [ParkingLot1][Joanna] refactor: ParkingLot.java

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,23 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
-    private int slotNumber = 0;
-    private int maxSlotNumber = 10;
+    private final static int DEFAULT_CAPACITY = 10;
+    private Map<ParkingTicket, Car> parkedPosition;
+    private int capacity;
+
+    public ParkingLot(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ParkingLot(int capacity){
+        parkedPosition = new HashMap<>();
+        this.capacity = capacity;
+    }
 
     public ParkingTicket park(Car car) {
-        if(slotNumber < maxSlotNumber){
-            ParkingTicket parkingTicket = new ParkingTicket();
-            parkedPosition.put(parkingTicket, car);
-            slotNumber++;
-            return parkingTicket;
+        if(isFull()){
+            return null;
         }
-        return null;
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkedPosition.put(parkingTicket, car);
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         final Car car = parkedPosition.get(parkingTicket);
         parkedPosition.remove(parkingTicket);
         return car;
+    }
+
+    private boolean isFull() {
+        return parkedPosition.size() >= capacity;
     }
 }
